4748e92b8254295966c390ce09f9a238decfe45a
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-04-04 10:57:10 -0300

    g

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 
 const alturaStatusBar = StatusBar.currentHeight
-const KEY_GPT = 'SUA_CHAVE';
+const KEY_GPT = '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';
 
 export default function App() {
 
